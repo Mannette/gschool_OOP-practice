b07fad6f85cd01d6fc23b91ea13d124303b0e265
ethan <ethanmannette@gmail.com>
2015-08-05 15:22:48 -0600

    Finished OOP practice/example problems

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ console.log('sanity check');
 
 
 // Base Class - constructor
-var Vehicle = function(numberOfWheels, carColor) {
+var Vehicle = function(wheels, color) {
   // properties
-  this.wheels = numberOfWheels;
-  this.color = carColor;
+  this.wheels = wheels;
+  this.color = color;
 };
 
 // Methods
@@ -28,13 +28,36 @@ Vehicle.prototype.toString = function() {
   return 'The vehicle has ' + this.wheels + ' wheels and is ' + this.color + '.';
 };
 
-// Instances
-var redCar = new Vehicle(4, 'red');
+// Car Class
+var Car = function(color) {
+  this.wheels = 4;
+  this.color = color;
+};
+
+// Sets car sub-class to inherit from Vehicle base class
+Car.prototype = new Vehicle();
+Car.prototype.drive = function(distance) {
+  return "The car traveled " + distance + " miles!";
+};
+Car.prototype.honk = function() {
+  return 'car honk!';
+};
+
+// Instance of the Car sub-class
+var redCar = new Car(4, 'red');
 console.log(redCar);
 console.log(redCar.honk());
+console.log(redCar.drive(10));
 console.log(redCar.toString());
 
-// challenges
+// Instance of the Vehicle base class
+var smallMotorcycle = new Vehicle(2, 'black');
+console.log(smallMotorcycle);
+console.log(smallMotorcycle.honk());
+console.log(smallMotorcycle.toString());
+
+//------------- CHALLENGES --------------------
+// ** challenge 1 **
 var Song = function(songName, bandName) {
   // properties
   this.song = songName;
@@ -43,7 +66,7 @@ var Song = function(songName, bandName) {
 
 // Methods
 Song.prototype.countName = function() {
-  return this.song.split('').length;
+  return this.song.split(' ').length;
 };
 Song.prototype.createElement = function() {
   return $('<div>').text(this.song + " by " + this.band);
@@ -56,3 +79,44 @@ console.log(music);
 console.log(music.countName());
 console.log(music.createElement());
 $('body').append(music.createElement());
+
+// ** challenge 2 **
+// create methods..
+  // getFullName()
+  // getFirstName()
+  // getLastName()
+var Person = function(firstAndLastName) {
+  this.firstAndLastName = firstAndLastName;
+  this.isInstructor = true;
+};
+
+// Person class methods
+Person.prototype.getFullName = function() {
+  return this.firstAndLastName;
+};
+Person.prototype.getFirstName = function() {
+  return this.firstAndLastName.split(' ')[0];
+};
+Person.prototype.getLastName = function() {
+  return this.firstAndLastName.split(' ')[1];
+};
+
+// student sub-class
+var Student = function(firstAndLastName) {
+  this.firstAndLastName = firstAndLastName;
+  this.isInstructor = false;
+};
+Student.prototype = new Person();
+
+
+var michael = new Person('Michael Herman');
+console.log(michael);
+console.log(michael.getFullName());
+console.log(michael.getFirstName());
+console.log(michael.getLastName());
+
+var ethan = new Student('Ethan Mannette');
+console.log(ethan);
+console.log(ethan.getFullName());
+console.log(ethan.getFirstName());
+console.log(ethan.getLastName());
